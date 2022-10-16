49f48a1b3e2fe72332874696a8108bab4dd8487b
Anton <anonymousStyleKiller@gmail.com>
2022-10-16 22:19:33 +0300

    feature(Solution): Base creating object with Activator

---
+++ ObjectFactoryWithExpression/Models/Cat.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectFactoryWithExpression.Models;
+
+public class Cat
+{
+    public Cat(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
+}
--- ObjectFactoryWithExpression/Program.cs
+++ ObjectFactoryWithExpression/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using ObjectFactoryWithExpression.Models;
+
+var list = new List<Cat>();
+var stopwatch = Stopwatch.StartNew();
+for (var i = 0; i < 100000; i++)
+{
+    var cat = Activator.CreateInstance<Cat>();
+    list.Add(cat);
+}
+
+Console.WriteLine($"{stopwatch.Elapsed} - Activator");
